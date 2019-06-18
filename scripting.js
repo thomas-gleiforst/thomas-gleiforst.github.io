7c8c5fb8fac0def3b1e42af745b9f01b6bd2aa9a
ps2dude7 <ps2dude67@sbcglobal.net>
2019-06-18 07:37:47 -0500

    Added open and closed menu images
    Added the image files, replaced the menu text on index.html with the menu icon, and added the javascript to switch between the two. Still not tested.

--- scripting.js
+++ scripting.js
@@ -4,6 +4,16 @@ jQuery(document).ready(function() {
 
     jQuery(".nav-menu").slideToggle(400, function() {
 
+      var menu = document.getElementById("menu-button");
+      if (menu.src == "closedMenu.png"){
+        menu.src = "openMenu.png";
+        menu.alt = "Open Menu";
+      }
+      else {
+        menu.src = "closedMenu.png";
+        menu.alt = "Closed Menu";
+      }
+
       jQuery(this).toggleClass("nav-expanded").css('display', '');
 
     });
